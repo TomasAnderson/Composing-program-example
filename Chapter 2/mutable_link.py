25975df7b08bdf9ba05338a9a005d98954a4a17d
Zhou You <yollyhideout@gmail.com>
2016-04-23 10:51:41 +0800

    dictionary and dispatch dictionary

---
+++ Chapter 2/mutable_link.py
@@ -0,0 +1,26 @@
+def mutable_link():
+  """Return a functional implementation of a mutabe linked list"""
+  contents = empty
+  def dispatch(message, value=None):
+    nonlocal contents
+    if message == 'len':
+      return len_link(contents)
+    elif message == 'getitem':
+      return getitem_link(contents, value)
+    elif message == 'push_first':
+      contents = link(value, contents)
+    elif message == 'pop_first':
+      f = first(contents)
+      contents = rest(contents)
+      return f
+    elif message == 'str':
+      return join_link(contents, ', ')
+  return dispatch
+
+def to_mutable_link(source):
+  """Return a functional list with the same contents as source"""
+  a = mutable_link()
+  for element in reversed(source):
+    s('push_first', element)
+  return s
+  
